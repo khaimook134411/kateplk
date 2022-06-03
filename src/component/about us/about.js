e9cd9dfe8de55bab9de77c0c6dcb8e39dac0adf3
khaimook134411 <78730962+khaimook134411@users.noreply.github.com>
2022-06-03 14:23:01 +0700

    about-us

--- src/component/about us/about.js
+++ src/component/about us/about.js
@@ -7,7 +7,7 @@ export default function About() {
             <div className={style.conRow1}>
                 <div className={style.conContent}>
                     <div className={style.headText}>
-                        Festo - Pnuematic and Electrical Automation System
+                        Festo - Pneumatic and Electrical Automation System
                     </div>
                     <div className={style.content}>
                             บริษัท เคท พีแอลเค จำกัด ผู้แทนจำหน่าย Festo - Pnuematic and Electrical Automation System สินค้าลิขสิทธิ์แท้ ถูกต้อง สินค้าทุกชิ้นพร้อมส่งอย่างรวดเร็ว พร้อมบริการให้คำปรึกษา แนะนำโดยทีมงานผู้เชี่ยวชาญด้าน Pnuematic and Electrical Automation ด้านนิวเมติกส์ Festo อันดับต้นๆของประเทศ เรายินดีให้บริการทุกท่าน ด้วยความเต็มใจ สอบถามข้อมูล โทร 081 615 7464 หรือเมล katesaraporn@kateplk.co.th
